4da8520f63d5f1844306ae6dadf7710fcad6736b
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-10-18 18:57:37 +0100

    Initialised the repo and tidied up the NPX package.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import'../styles/app.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Surreal Estate
         </a>
       </header>
     </div>
